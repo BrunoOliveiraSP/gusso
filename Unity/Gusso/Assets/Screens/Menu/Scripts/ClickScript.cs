5573345522144d8639cf8a2bdf4900df75848263
Bruno de Oliveira <brunooliveira.me@outlook.com>
2018-06-06 17:52:32 -0300

    telas menu e game2 em andamento

---
+++ Unity/Gusso/Assets/Screens/Menu/Scripts/ClickScript.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ClickScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        if (gameObject.name == "game1")
+        {
+            SceneManager.LoadScene("Game1");
+        }
+        else if (gameObject.name == "game2")
+        {
+            SceneManager.LoadScene("Game2");
+        }
+        else if (gameObject.name == "game3")
+        {
+            SceneManager.LoadScene("Game3");
+        }
+        else if (gameObject.name == "sair")
+        {
+            Application.Quit();
+        }
+    }
+}
