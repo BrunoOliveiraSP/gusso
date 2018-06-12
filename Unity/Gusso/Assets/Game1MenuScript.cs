69e838b9d9188ce6bce5f01450e620557846a1f1
Bruno de Oliveira <brunooliveira.me@outlook.com>
2018-06-12 11:55:07 -0300

    game 1 90%

---
+++ Unity/Gusso/Assets/Game1MenuScript.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Game1MenuScript : MonoBehaviour {
+
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
