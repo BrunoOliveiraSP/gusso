0db1a20fc968082b67b98a75d1d5c211d9296bee
Bruno de Oliveira <brunooliveira.me@outlook.com>
2018-06-12 21:46:34 -0300

    easter egg

---
+++ Unity/Gusso/Assets/EasterEgg.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEgg : MonoBehaviour {
+
+    public GameObject Menu;
+
+    bool comecou = false;
+	void Update () {
+
+        if (Input.touchCount == 4 /*|| Input.GetMouseButtonDown(0)*/)
+        {
+            foreach (Transform child in Menu.transform)
+            {
+                child.gameObject.GetComponent<Renderer>().enabled = false;
+            }
+
+            comecou = true;
+            gameObject.transform.position = new Vector3(-7f, gameObject.transform.position.y);
+        }
+
+        if (comecou == true)
+        {
+            float speed = 3.0f;
+            gameObject.transform.Translate(Vector3.right * speed * Time.deltaTime);
+        }
+
+        if (transform.position.x > 36f)
+        {
+            foreach (Transform child in Menu.transform)
+            {
+                child.gameObject.GetComponent<Renderer>().enabled = true;
+            }
+            comecou = false;
+        }
+
+
+    }
+}
